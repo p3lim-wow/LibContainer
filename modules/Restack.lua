33224505f3f41366e008a066d4bc968e53cefc3b
Adrian L Lange <mail@p3lim.net>
2016-10-07 07:46:52 +0200

    Swanky new restack textures

--- modules/Restack.lua
+++ modules/Restack.lua
@@ -2,15 +2,27 @@ local P = unpack(select(2, ...))
 
 local function Init(self)
 	local Button = CreateFrame('Button', '$parentRestack', self)
-	Button:SetPoint('TOPRIGHT', -8, -8)
+	Button:SetPoint('TOPRIGHT', -9, -6)
 	Button:SetSize(16, 16)
 	Button:SetScript('OnClick', SortBags)
 	self.Restack = Button
 
-	local Texture = Button:CreateTexture('$parentTexture', 'ARTWORK')
-	Texture:SetAllPoints()
-	Texture:SetTexture([[Interface\Buttons\UI-GroupLoot-Pass-Up]])
-	Button.Texture = Texture
+	local NormalTexture = Button:CreateTexture('$parentNormal', 'ARTWORK')
+	NormalTexture:SetAllPoints()
+	NormalTexture:SetAtlas('bags-button-autosort-up')
+
+	local PushedTexture = Button:CreateTexture('$parentPushed', 'ARTWORK')
+	PushedTexture:SetAllPoints()
+	PushedTexture:SetAtlas('bags-button-autosort-down')
+
+	local HighlightTexture = Button:CreateTexture('$parentPushed', 'ARTWORK')
+	HighlightTexture:SetAllPoints()
+	HighlightTexture:SetTexture([[Interface\Buttons\ButtonHilight-Square]])
+	HighlightTexture:SetBlendMode('ADD')
+
+	Button:SetNormalTexture(NormalTexture)
+	Button:SetPushedTexture(PushedTexture)
+	Button:SetHighlightTexture(HighlightTexture)
 
 	P.Fire('PostCreateRestack', self)
 end
