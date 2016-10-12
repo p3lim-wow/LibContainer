fe06b8ca83cb6a3fdd4fd8fc0b6ef95e33fb429a
Adrian L Lange <mail@p3lim.net>
2016-10-12 13:59:09 +0200

    Improve output functions

--- core/Private.lua
+++ core/Private.lua
@@ -3,10 +3,15 @@ private[2] = newproxy(true) -- E: events
 private[1] = {name = addonName} -- P: private
 
 local P = private[1]
-function P.error(...)
-	print(...)
+function P.print(...)
+	print('|cff33ff99Backpack|r', ...)
 end
 
-function P.print(...)
-	print(...)
+local lastMessage
+function P.error(message, ...)
+	if(message ~= lastMessage) then
+		P.print('|cffff0000Error:|r ' .. string.format(message, ...))
+		lastMessage = message
+	end
 end
+
