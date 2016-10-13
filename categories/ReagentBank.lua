116ff5332ea4defc13622e4f7b2082f4d7ad8e8c
Adrian L Lange <mail@p3lim.net>
2016-10-13 21:27:15 +0200

    Add a deposit all reagents button

--- categories/ReagentBank.lua
+++ categories/ReagentBank.lua
@@ -8,3 +8,22 @@ local categoryFilter = function(bagID, slotID, itemID)
 end
 
 Backpack:AddCategory(categoryIndex, categoryName, categoryFilter)
+
+local function ShowDepositReagentsTooltip(self)
+	GameTooltip:SetOwner(self, 'ANCHOR_TOP')
+	GameTooltip:SetText(REAGENTBANK_DEPOSIT)
+	GameTooltip:Show()
+end
+
+Backpack:AddModule('DepositReagents', function()
+	local Button = CreateFrame('Button', '$parentDepositReagents', BackpackBankContainerReagentBank)
+	Button:SetPoint('TOPRIGHT', -8, -6)
+	Button:SetSize(16, 16)
+	Button:SetScript('OnClick', DepositReagentBank)
+	Button:SetScript('OnEnter', ShowDepositReagentsTooltip)
+	Button:SetScript('OnLeave', GameTooltip_Hide)
+
+	local Texture = Button:CreateTexture('$parentTexture', 'ARTWORK')
+	Texture:SetAllPoints()
+	Texture:SetTexture([[Interface\Buttons\UI-GroupLoot-Pass-Up]])
+end)
