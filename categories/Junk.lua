1233e7599ede7301a9bce4cdb74ec0a83482f4d1
Adrian L Lange <mail@p3lim.net>
2016-10-13 12:41:32 +0200

    Mix modules into categories that are dependant on them
    Maybe a bit messy, but this clearly separates true modules from extra features for existing categories

--- categories/Junk.lua
+++ categories/Junk.lua
@@ -1,3 +1,5 @@
+local P = unpack(select(2, ...))
+
 local categoryName = BAG_FILTER_JUNK -- "Junk"
 local categoryIndex = 1e3
 
@@ -12,3 +14,43 @@ local categoryFilter = function(bagID, slotID, itemID)
 end
 
 Backpack:AddCategory(categoryIndex, categoryName, categoryFilter)
+
+local lastNumItems = 0
+local function Update(event)
+	if(BackpackDB.autoSellJunk) then
+		if(event == 'MERCHANT_SHOW' or lastNumItems > 0) then
+			lastNumItems = 0
+
+			local Container = P.GetCategoryContainer(Backpack, 1e3)
+			for _, Slot in next, Container.slots do
+				lastNumItems = lastNumItems + 1
+				UseContainerItem(Slot.bagID, Slot.slotID)
+			end
+		end
+	end
+end
+
+local function OnClick(self)
+	BackpackDB.autoSellJunk = not BackpackDB.autoSellJunk
+	self.Texture:SetDesaturated(not BackpackDB.autoSellJunk)
+end
+
+local function Init(self)
+	local Parent = BackpackContainerJunk
+
+	local Button = CreateFrame('Button', '$parentToggleSellJunk', Parent)
+	Button:SetPoint('TOPRIGHT', -8, -8)
+	Button:SetSize(16, 16)
+	Button:SetScript('OnClick', OnClick)
+	self.SellJunk = Button
+
+	local Texture = Button:CreateTexture('$parentTexture', 'ARTWORK')
+	Texture:SetAllPoints()
+	Texture:SetTexture([[Interface\Buttons\UI-GroupLoot-Pass-Up]])
+	Texture:SetDesaturated(not BackpackDB.autoSellJunk)
+	Button.Texture = Texture
+
+	P.Fire('PostCreateSellJunk', self)
+end
+
+P.AddModule(Init, Update, false, 'MERCHANT_SHOW', 'MERCHANT_CLOSED', 'BAG_UPDATE_DELAYED')
--- categories/New.lua
+++ categories/New.lua
@@ -1,3 +1,5 @@
+local P = unpack(select(2, ...))
+
 local categoryName = NEW .. ' ' .. ITEMS -- TODO: improve this
 local categoryIndex = 1001
 
@@ -15,3 +17,35 @@ local categoryFilter = function(bagID, slotID, itemID)
 end
 
 Backpack:AddCategory(categoryIndex, categoryName, categoryFilter)
+
+local function OnClick(self)
+	local Container = P.GetCategoryContainer(self.parentContainer, 1001)
+	for _, Slot in next, Container.slots do
+		local itemID = Slot.itemID
+		BackpackKnownItems[itemID] = true
+		BackpackCustomCategory[itemID] = nil
+	end
+
+	P.UpdateAllSlots('ResetNew')
+	P.PositionSlots()
+end
+
+local function Init(self)
+	local Parent = _G[self:GetName() .. 'ContainerNewItems']
+
+	local Button = CreateFrame('Button', '$parentResetNew', Parent)
+	Button:SetPoint('TOPRIGHT', -8, -8)
+	Button:SetSize(16, 16)
+	Button:SetScript('OnClick', OnClick)
+	Button.parentContainer = self
+	self.ResetNew = Button
+
+	local Texture = Button:CreateTexture('$parentTexture', 'ARTWORK')
+	Texture:SetAllPoints()
+	Texture:SetTexture([[Interface\Buttons\UI-RefreshButton]])
+	Button.Texture = Texture
+
+	P.Fire('PostCreateResetNew', self)
+end
+
+P.AddModule(Init, nil, true)
