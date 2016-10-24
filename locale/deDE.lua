9c528a6b3c2c500e39846b1192f3042ae5ae262e
bullsei <bullsei@users.noreply.github.com>
2016-10-24 14:27:36 +0200

    Update deDE.lua

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -5,30 +5,30 @@ L['Artifact Power'] = ARTIFACT_POWER
 L['Collections'] = COLLECTIONS
 L['Consumables'] = BAG_FILTER_CONSUMABLES
 L['Equipment'] = BAG_FILTER_EQUIPMENT
-L['Equipment Sets'] = 'Equipment Sets'
+L['Equipment Sets'] = 'Ausrüstungssets'
 L['World Event'] = BATTLE_PET_SOURCE_7
-L['Erroneous'] = 'Erroneous'
+L['Erroneous'] = 'Fehlerhaft'
 L['Inventory'] = INVENTORY_TOOLTIP
 L['Junk'] = BAG_FILTER_JUNK
-L['New Items'] = 'New Items'
+L['New Items'] = 'Neue Gegenstände'
 L['Professions'] = TRADE_SKILLS
 L['Quests Items'] = AUCTION_CATEGORY_QUEST_ITEMS
 L['Reagent Bank'] = REAGENT_BANK
-L['Teleporters'] = 'Teleporters'
+L['Teleporters'] = 'Teleporter'
 L['Trade Goods'] = AUCTION_CATEGORY_TRADE_GOODS
 
 -- Container button tooltips
-L['Mark items as known'] = 'Mark items as known'
-L['Toggle auto-deposit'] = 'Toggle auto-deposit'
-L['Toggle bag slots'] = 'Toggle bag slots'
+L['Mark items as known'] = 'Gegenstände als bekannt markieren'
+L['Toggle auto-deposit'] = 'Automatische Einlagerung ein-/ausschalten'
+L['Toggle bag slots'] = 'Taschenplätze umschalten'
 
 -- Error messages
-L['Enabled category "%s"'] = 'Enabled category "%s"'
-L['Disabled category "%s"'] = 'Disabled category "%s"'
-L['Category "%s" is already enabled'] = 'Category "%s" is already enabled'
-L['Category "%s" is already disabled'] = 'Category "%s" is already disabled'
-L['Can\'t disable protected category "%s"'] = 'Can\'t disable protected category "%s"'
-L['Found no category with index %s'] = 'Found no category with index %s'
-L['/reload for changes to take effect'] = '/reload for changes to take effect'
-L['A layout already exists (%s)'] = 'A layout already exists (%s)'
-L['Override for "%s" already exists'] = 'Override for "%s" already exists'
+L['Enabled category "%s"'] = 'Kategorie "%s" wurde aktiviert'
+L['Disabled category "%s"'] = 'Kategorie "%s" wurde deaktiviert'
+L['Category "%s" is already enabled'] = 'Kategorie "%s" ist bereits aktiviert'
+L['Category "%s" is already disabled'] = 'Kategorie "%s" ist bereits deaktiviert'
+L['Can\'t disable protected category "%s"'] = 'Die geschützte Kategorie "%s" kann nicht deaktiviert werden'
+L['Found no category with index %s'] = 'Es wurde keine Kategorie mit dem Index %s gefunden'
+L['/reload for changes to take effect'] = '/reload, um die Änderungen zu übernehmen'
+L['A layout already exists (%s)'] = 'Ein Layout existiert bereits (%s)'
+L['Override for "%s" already exists'] = 'Eine Überschreibung für "%s" existiert bereits'
