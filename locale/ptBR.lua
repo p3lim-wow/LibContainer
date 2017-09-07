8d650b934327bc2fab6e3fe03bf4b09337db2c7c
Cazzbr <31737100+Cazzbr@users.noreply.github.com>
2017-09-07 12:27:23 -0300

    Update ptBR.lua (#40)
    As far I could find, there is only the "Toggle auto-vendoring" at the junk bag missing translation, but It's a skin specific feature, am I right?

--- locale/ptBR.lua
+++ locale/ptBR.lua
@@ -5,25 +5,25 @@ L['Artifact Power'] = ARTIFACT_POWER
 L['Collections'] = COLLECTIONS
 L['Consumables'] = BAG_FILTER_CONSUMABLES
 L['Equipment'] = BAG_FILTER_EQUIPMENT
-L['Equipment Sets'] = 'Equipment Sets' -- needs translation
+L['Equipment Sets'] = 'Conjunto de Equipamentos' -- needs translation (done)
 L['World Event'] = BATTLE_PET_SOURCE_7
-L['Erroneous'] = 'Erroneous' -- needs translation
+L['Erroneous'] = 'Errôneo' -- needs translation (done)
 L['Inventory'] = INVENTORY_TOOLTIP
 L['Junk'] = BAG_FILTER_JUNK
-L['New Items'] = 'New Items' -- needs translation
+L['New Items'] = 'Novos Itens' -- needs translation (done)
 L['Professions'] = TRADE_SKILLS
 L['Quests Items'] = AUCTION_CATEGORY_QUEST_ITEMS
 L['Reagent Bank'] = REAGENT_BANK
-L['Teleporters'] = 'Teleporters' -- needs translation
+L['Teleporters'] = 'Teleportes' -- needs translation (done)
 L['Trade Goods'] = AUCTION_CATEGORY_TRADE_GOODS
 
 -- Container button tooltips
-L['Mark items as known'] = 'Mark items as known' -- needs translation
-L['Toggle auto-deposit'] = 'Toggle auto-deposit' -- needs translation
-L['Toggle bag slots'] = 'Toggle bag slots' -- needs translation
-L['Restack'] = 'Restack' -- needs translation
-L['Restack Bank'] = 'Restack Bank' -- needs translation
-L['Restack Reagent Bank'] = 'Restack Reagent Bank' -- needs translation
+L['Mark items as known'] = 'Marcar itens como conhecidos' -- needs translation (done)
+L['Toggle auto-deposit'] = 'Ativar auto-deposito' -- needs translation (done)
+L['Toggle bag slots'] = 'Mostrar Bolsas' -- needs translation (done)
+L['Restack'] = 'Reorganizar' -- needs translation (done)
+L['Restack Bank'] = 'Reorganizar Banco' -- needs translation (done)
+L['Restack Reagent Bank'] = 'Reorganizar Banco de Reagentes' -- needs translation (done)
 L['Deposit All Reagents'] = REAGENTBANK_DEPOSIT
 L['Lock'] = LOCK
 L['Unlock'] = UNLOCK
@@ -33,9 +33,9 @@ L['Purchase'] = PURCHASE
 L['Equip Container'] = EQUIP_CONTAINER
 
 -- Error messages
-L['A layout already exists (%s)'] = 'A layout already exists (%s)' -- needs translation
-L['Override for "%s" already exists'] = 'Override for "%s" already exists' -- needs translation
+L['A layout already exists (%s)'] = 'Um layout já existe (%s)' -- needs translation (done)
+L['Override for "%s" already exists'] = 'Substituir por "%s" já existe' -- needs translation (done)
 
 -- Config
-L['Here you can toggle categories or even create new ones!'] = 'Here you can toggle categories or even create new ones!' -- needs translation
-L['Modifier to show bank when toggling Backpack'] = 'Modifier to show bank when toggling Backpack' -- needs translation
+L['Here you can toggle categories or even create new ones!'] = 'Aqui você pode ativar/desativar categorias ou até criar novas!' -- needs translation (done)
+L['Modifier to show bank when toggling Backpack'] = 'Modificador para mostrar o banco quando abrir a Mochila' -- needs translation (done)
