924778d03b1344dd59afb2de4eeb7085d41215d2
Adrian L Lange <mail@p3lim.net>
2017-09-07 17:27:59 +0200

    ptBR cleanup

--- locale/ptBR.lua
+++ locale/ptBR.lua
@@ -5,25 +5,25 @@ L['Artifact Power'] = ARTIFACT_POWER
 L['Collections'] = COLLECTIONS
 L['Consumables'] = BAG_FILTER_CONSUMABLES
 L['Equipment'] = BAG_FILTER_EQUIPMENT
-L['Equipment Sets'] = 'Conjunto de Equipamentos' -- needs translation (done)
+L['Equipment Sets'] = 'Conjunto de Equipamentos'
 L['World Event'] = BATTLE_PET_SOURCE_7
-L['Erroneous'] = 'Errôneo' -- needs translation (done)
+L['Erroneous'] = 'Errôneo'
 L['Inventory'] = INVENTORY_TOOLTIP
 L['Junk'] = BAG_FILTER_JUNK
-L['New Items'] = 'Novos Itens' -- needs translation (done)
+L['New Items'] = 'Novos Itens'
 L['Professions'] = TRADE_SKILLS
 L['Quests Items'] = AUCTION_CATEGORY_QUEST_ITEMS
 L['Reagent Bank'] = REAGENT_BANK
-L['Teleporters'] = 'Teleportes' -- needs translation (done)
+L['Teleporters'] = 'Teleportes'
 L['Trade Goods'] = AUCTION_CATEGORY_TRADE_GOODS
 
 -- Container button tooltips
-L['Mark items as known'] = 'Marcar itens como conhecidos' -- needs translation (done)
-L['Toggle auto-deposit'] = 'Ativar auto-deposito' -- needs translation (done)
-L['Toggle bag slots'] = 'Mostrar Bolsas' -- needs translation (done)
-L['Restack'] = 'Reorganizar' -- needs translation (done)
-L['Restack Bank'] = 'Reorganizar Banco' -- needs translation (done)
-L['Restack Reagent Bank'] = 'Reorganizar Banco de Reagentes' -- needs translation (done)
+L['Mark items as known'] = 'Marcar itens como conhecidos'
+L['Toggle auto-deposit'] = 'Ativar auto-deposito'
+L['Toggle bag slots'] = 'Mostrar Bolsas'
+L['Restack'] = 'Reorganizar'
+L['Restack Bank'] = 'Reorganizar Banco'
+L['Restack Reagent Bank'] = 'Reorganizar Banco de Reagentes'
 L['Deposit All Reagents'] = REAGENTBANK_DEPOSIT
 L['Lock'] = LOCK
 L['Unlock'] = UNLOCK
@@ -33,9 +33,9 @@ L['Purchase'] = PURCHASE
 L['Equip Container'] = EQUIP_CONTAINER
 
 -- Error messages
-L['A layout already exists (%s)'] = 'Um layout já existe (%s)' -- needs translation (done)
-L['Override for "%s" already exists'] = 'Substituir por "%s" já existe' -- needs translation (done)
+L['A layout already exists (%s)'] = 'Um layout já existe (%s)'
+L['Override for "%s" already exists'] = 'Substituir por "%s" já existe'
 
 -- Config
-L['Here you can toggle categories or even create new ones!'] = 'Aqui você pode ativar/desativar categorias ou até criar novas!' -- needs translation (done)
-L['Modifier to show bank when toggling Backpack'] = 'Modificador para mostrar o banco quando abrir a Mochila' -- needs translation (done)
+L['Here you can toggle categories or even create new ones!'] = 'Aqui você pode ativar/desativar categorias ou até criar novas!'
+L['Modifier to show bank when toggling Backpack'] = 'Modificador para mostrar o banco quando abrir a Mochila'
