fb4f60072afa257b92d931da2b1c543f5237f8fe
Adrian L Lange <mail@p3lim.net>
2018-07-02 18:53:58 +0200

    Add money widget

---
+++ widgets/Money.lua
@@ -0,0 +1,34 @@
+local L = LibContainer.locale
+
+local function Update(self)
+	local money = GetMoney()
+	local output = string.format('|cffffff66%d|r', math.floor(money / 1e4))
+	output = string.format('%s.|cffc0c0c0%d|r', output, math.floor((money / 1e2) % 1e2))
+	output = string.format('%s.|cffcc9900%d|r', output, math.floor(money % 1e2))
+
+	local Label = self.Label
+	Label:SetText(output)
+
+	self:SetSize(Label:GetSize())
+end
+
+local function Enable(self)
+	if(not self.Label) then
+		local Label = self:CreateFontString(nil, 'ARTWORK', 'GameFontHighlightSmall')
+		Label:SetPoint('BOTTOMRIGHT')
+		self.Label = Label
+	end
+
+	self:SetScript('OnClick', MoneyInputFrame_OpenPopup)
+	self.info = MoneyTypeInfo.PLAYER
+
+	self:RegisterEvent('PLAYER_MONEY')
+	self:RegisterEvent('PLAYER_LOGIN')
+end
+
+local function Disable(self)
+	self:UnregisterEvent('PLAYER_MONEY')
+	self:UnregisterEvent('PLAYER_LOGIN')
+end
+
+LibContainer:RegisterWidget('Money', Enable, Disable, Update)
